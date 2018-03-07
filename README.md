47a8d808b91057b2aed5bc4156c8d58e13f73b1e
Johnny Wu <karasaya@gmail.com>
2018-03-07 17:29:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## vtween
+
+Tween animation engine
+
+## Install
+
+```bash
+npm install vtween
+```
+
+## Usage
+
+```javascript
+vtween(ent, {
+  position: [0, 20, 0]
+}, {
+  duration: 1800
+});
+```
+
+## Documentation
+
+TODO
+
+## License
+
+MIT © 2017 gamedev-js
---
+++ script/rollup.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const buble = require('rollup-plugin-buble');
+const fsJetpack = require('fs-jetpack');
+const pjson = require('../package.json');
+
+let banner = `
+/*
+ * ${pjson.name} v${pjson.version}
+ * (c) ${new Date().getFullYear()} @gamedev-js
+ * Released under the MIT License.
+ */
+`;
+
+let dest = './dist';
+let file = 'vtween';
+let name = 'vtween';
+let sourcemap = true;
+let globals = {};
+
+// clear directory
+fsJetpack.dir(dest, { empty: true });
+
+module.exports = {
+  input: './index.js',
+  external: [],
+  plugins: [
+    buble(),
+  ],
+  output: [
+    {
+      file: `${dest}/${file}.dev.js`,
+      format: 'iife',
+      name,
+      banner,
+      globals,
+      sourcemap
+    },
+    {
+      file: `${dest}/${file}.js`,
+      format: 'cjs',
+      name,
+      banner,
+      globals,
+      sourcemap
+    },
+  ],
+};
