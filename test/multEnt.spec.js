4dc4276748132646eef04a83f9303bb018aa6207
PYZ <592744797@qq.com>
2018-03-13 13:37:36 +0800

    third commit

--- test/multEnt.spec.js
+++ test/multEnt.spec.js
@@ -4,47 +4,47 @@ const vmath = require('vmath');
 const { vec2, vec3 } = vmath;
 
 class entity {
-	constructor(params = {}) {
-		this.lfloat = params.lfloat;
-		this.lvec2 = params.lvec2;
-		this.lvec3 = params.lvec3;
-	}
+  constructor(params = {}) {
+    this.lfloat = params.lfloat;
+    this.lvec2 = params.lvec2;
+    this.lvec3 = params.lvec3;
+  }
 }
 
 tap.test('mult_ent', t => {
 
-	let vec2_a, vec3_a;
-	vec2_a = vec2.create();
-	vec2.set(vec2_a, 1, 1);
+  let vec2_a, vec3_a;
+  vec2_a = vec2.create();
+  vec2.set(vec2_a, 1, 1);
 
 
-	vec3_a = vec3.create();
-	vec3.set(vec3_a, 1, 1, 1);
+  vec3_a = vec3.create();
+  vec3.set(vec3_a, 1, 1, 1);
 
-	let ent_prop = {
-		lfloat: 1,
-		lvec2: vec2_a,
-		lvec3: vec3_a
-	}
+  let ent_prop = {
+    lfloat: 1,
+    lvec2: vec2_a,
+    lvec3: vec3_a
+  }
 
 
-	let ent_1 = new entity(ent_prop);
-	let ent_2 = new entity(ent_prop);
+  let ent_1 = new entity(ent_prop);
+  let ent_2 = new entity(ent_prop);
 
-	console.log(ent_1.lvec3);
-	console.log(ent_2.lvec3);
+  console.log(ent_1.lvec3);
+  console.log(ent_2.lvec3);
 
-	let vt = new vtween({
-		targets: [ent_1, ent_2],
-		anim: {
-			lvec3: [0, 0, 0],
-		},
-	});
-	vt.tick(500);
-	vt.tick(1000);
+  let vt = new vtween({
+    targets: [ent_1, ent_2],
+    anim: {
+      lvec3: [0, 0, 0],
+    },
+  });
+  vt.tick(500);
+  vt.tick(1000);
 
-	console.log(ent_1.lvec3);
-	console.log(ent_2.lvec3);
-	t.end();
+  console.log(ent_1.lvec3);
+  console.log(ent_2.lvec3);
+  t.end();
 
 })
